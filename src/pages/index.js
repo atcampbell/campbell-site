98b28543f26d415c0ea34d929f7d0891d48a8786
atcampbell <adamtoddcampbell@gmail.com>
2020-04-09 19:34:46 +0100

    simple page set up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,75 @@
-import React from "react"
+import React from 'react';
+import Layout from '../components/Layout';
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <Layout>
+        <div>
+            <h1>Adam Campbell</h1>
+            <p>
+                <span>Musician and software developer from Glasgow.</span>
+                <span>
+                    <a href="https://twitter.com/atttcampbell">Twitter</a>
+                </span>
+            </p>
+            <h2>Current:</h2>
+            <p>
+                <span>
+                    <a href="https://www.fancyyyyy.com">FANCYYYYY</a> - record label and eurorack
+                    modules with Tristan Clutterbuck
+                </span>
+                <span>Ego Depletion with Fritz Welch</span>
+                <span>Fua with Tina Krekels and Tristan Clutterbuck</span>
+            </p>
+            <h2>Recent:</h2>
+            <p>
+                <span>
+                    <a href="http://chocolatemonk.co.uk/available.html">Ego Depletion: EGO DERPZ</a>{' '}
+                    - CD on Chocolate Monk
+                </span>
+                <span>
+                    <a href="https://www.dundee.ac.uk/cooper-gallery/events/scoringnoisebowgamelanensemblesymposium/">
+                        Scoring Noise | Bow Gamelan Ensemble Symposium, Cooper Gallery Dundee
+                    </a>{' '}
+                    - performance with Tina Krekels
+                </span>
+                <span>
+                    <a href="http://2018.tectonicsfestival.com/schedule/tony-bevan-miya-masaoka-tina-krekels-with-adam-campbell">
+                        Tectonics festival
+                    </a>{' '}
+                    - performance with Tina Krekels
+                </span>
+                <span>
+                    <a href="https://fancyyyyy.bandcamp.com/album/fua">Fua</a> - Cassette and
+                    download on Fancyyyyy
+                </span>
+                <span>
+                    <a href="http://alkualkualkualkualkualkualkualkualkualku.org/pmwiki/pmwiki.php/Main/ALKU141">
+                        Googiepal: The Scottish Rave Whistle and The Danish Homophobic Rave
+                        Recorders
+                    </a>{' '}
+                    - 7" on Alku
+                </span>
+                <span>
+                    <a href="https://digital.eca.ed.ac.uk/gap-in-air/2015/01/05/recurring-points-adam-campbell/">
+                        Gap in the Air, Talbot Rice Gallery
+                    </a>{' '}
+                    - Recurring Points: audio visual installation
+                </span>
+                <span>
+                    <a href="http://www.counterflows.com/residency-blog/">
+                        Counterflows / Goethe Institute
+                    </a>{' '}
+                    - festival residency with Andrea Neumann
+                </span>
+                <span>
+                    <a href="https://steim.org/">STEIM</a> - physical computing internship
+                </span>
+                <span>
+                    <a href="https://www.cafeoto.co.uk/">Café Oto / Jerwood Arts</a> - residency
+                    with Boar collective
+                </span>
+            </p>
+            <p></p>
+        </div>
+    </Layout>
+);
